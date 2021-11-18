b1ddca0d09dd2254af31943223b5481817e4364a
Alexander Barabanov <Sasha.bar99@gmail.com>
2021-11-18 17:36:02 +0300

    Update custom range function to take more parameters

--- homework2/task5.py
+++ homework2/task5.py
@@ -20,11 +20,23 @@ assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == /
 from typing import Iterable, List
 
 
-def custom_range(iterable: Iterable[any], n: any) -> List[any]:
-    ranged_list = list(iterable)[:list(iterable).index(n)]
-    return ranged_list
+def custom_range(iterable: Iterable[any], iter_range: any, *args) -> List[any]:
+    if not args:
+        return list(iterable)[:list(iterable).index(iter_range)]
+    if len(args) > 2:
+        args = args[:3]
+    if len(args) < 2:
+        step = 1
+    else:
+        step = args[1]
+    if args[0] > iter_range:
+        return list(iterable)[list(iterable).index(iter_range):
+                              list(iterable).index(args[0]):step]
+    else:
+        return list(iterable)[list(iterable).index(args[0]):
+                              list(iterable).index(iter_range):step]
 
 
 if __name__ == '__main__':
-    print(custom_range("abcdefghijklmnopqrstuvwxyz", "g"))
-    print(custom_range([1, 2, 3, 4, 5, 6, 7], 4))
+    print(custom_range("abcdefghijklmnopqrstuvwxyz", "g", "p", 2))
+    print(custom_range([1, 2, 3, 4, 5, 6, 7], 3, 6, 2))
