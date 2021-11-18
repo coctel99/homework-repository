5eca5088f70777221d754fd9f67582c87cfa8ded
Alexander Barabanov <Sasha.bar99@gmail.com>
2021-11-18 16:52:18 +0300

    Rework checking if fibonacci sequence algorithm for n < 3 elements

--- homework1/task2.py
+++ homework1/task2.py
@@ -4,13 +4,71 @@ Given a cell with "it's a fib sequence" from slideshow,
     and returns if the given sequence is a Fibonacci sequence
 We guarantee, that the given sequence contain >= 0 integers inside.
 """
+from math import log, sqrt
 from typing import Sequence
 
 
-def check_fibonacci(data: Sequence[int]) -> bool:
-    if len(data) < 3:
+def check_if_fibonacci_number(number: int) -> bool:
+    """
+
+    Check if integer is fibonacci number
+
+    Based on formulas of the Fibonacci number identification in the wiki,
+    x is a Fibonacci number if at list one of 5x^2 + 4 or 5x^2 - 4 is a
+    perfect square
+
+    :param number: A number to check
+    :return: Is a fibonacci number or not
+    """
+    if sqrt(5 * number ** 2 + 4).is_integer() or \
+            sqrt(5 * number ** 2 - 4).is_integer():
+        return True
+    else:
         return False
-    for i in range(len(data) - 2):
-        if not data[i + 2] == data[i + 1] + data[i]:
+
+
+def get_position_in_fibonacci_sequence(number: int) -> int:
+    """
+
+    Get the position of the Fibonacci number in the Fibonacci sequence
+
+    :param number: A Fibonacci number
+    :return: A position of the number in the Fibonacci sequence
+    """
+    if number != 0:
+        pos = round(
+            log((number * sqrt(5) + sqrt(5 * number ** 2 + 4)) / 2,
+                (1 + 5 ** 0.5) / 2)
+        )
+        return pos
+    return 1
+
+
+def check_if_fibonacci_sequence(data: Sequence[int]) -> bool:
+    """
+
+    Check if sequence is a Fibonacci sequence
+
+    :param data: Sequence to check
+    :return: Is sequence is a Fibonacci sequence or not
+    """
+    data = list(data)
+    if len(data) == 1:
+        if not check_if_fibonacci_number:
             return False
+    elif len(data) == 2:
+        if data == [1, 1]:
+            return True
+        if not (check_if_fibonacci_number(data[0]) and
+                check_if_fibonacci_number(data[1])):
+            return False
+        elif get_position_in_fibonacci_sequence(data[1]) \
+                - get_position_in_fibonacci_sequence(data[0]) != 1:
+            return False
+    elif len(data) > 2:
+        if data == [0, 1, 1]:
+            return True
+        for current in range(len(data) - 2):
+            if not data[current + 2] == data[current + 1] + data[current]:
+                return False
     return True
