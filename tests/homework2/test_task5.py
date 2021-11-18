4c3d098c16f91f655586b2d805518b181080eb66
Alexander Barabanov <Sasha.bar99@gmail.com>
2021-11-18 17:36:29 +0300

    Add tests for custom range function

--- tests/homework2/test_task5.py
+++ tests/homework2/test_task5.py
@@ -2,17 +2,44 @@ from homework2.task5 import custom_range
 
 
 def test_custom_range_string():
-    """Testing that custom range function works on string"""
+    """Testing that custom range function makes range from string"""
     assert custom_range("abcdefghijklmnopqrstuvwxyz",
                         "g") == ['a', 'b', 'c', 'd', 'e', 'f']
 
 
+def test_custom_range_string_2_param():
+    """Testing that custom range function makes range from one arg
+    to another from string"""
+    assert custom_range("abcdefghijklmnopqrstuvwxyz",
+                        "g", "p") == ['g', 'h', 'i', 'j', 'k',
+                                      'l', 'm', 'n', 'o']
+
+
+def test_custom_range_string_3_param():
+    """Testing that custom range function makes range from one arg
+    to another with specified step from string"""
+    assert custom_range("abcdefghijklmnopqrstuvwxyz",
+                        "g", "p", 2) == ['g', 'i', 'k', 'm', 'o']
+
+
 def test_custom_range_int_list():
-    """Testing that custom range function works on int list"""
-    assert custom_range([1, 2, 3, 4, 5], 3) == [1, 2]
+    """Testing that custom range function makes range from int list"""
+    assert custom_range([1, 2, 3, 4, 5, 6, 7], 4) == [1, 2, 3]
+
+
+def test_custom_range_int_list_2_param():
+    """Testing that custom range function makes range from one arg
+    to another from int list"""
+    assert custom_range([1, 2, 3, 4, 5, 6, 7], 3, 6) == [3, 4, 5]
+
+
+def test_custom_range_int_list_3_param():
+    """Testing that custom range function makes range from one arg
+    to another with specified step from int list"""
+    assert custom_range([1, 2, 3, 4, 5, 6, 7], 3, 6, 2) == [3, 5]
 
 
 def test_custom_range_string_list():
-    """Testing that custom range function works on string list"""
+    """Testing that custom range function makes range from string list"""
     assert custom_range(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
                         "g") == ['a', 'b', 'c', 'd', 'e', 'f']
