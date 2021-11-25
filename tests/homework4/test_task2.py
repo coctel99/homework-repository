e436195e7ddcbd0638b745525bb63fb0cbe2c464
Alexander Barabanov <Sasha.bar99@gmail.com>
2021-11-26 01:48:53 +0300

    Add test with mocked function to open url

---
+++ tests/homework4/test_task2.py
@@ -0,0 +1,23 @@
+import unittest.mock as mock
+
+from homework4.task2 import count_dots_on_i
+
+
+TEXT_STRING = "Some ii mocked text i"
+
+
+def fake_urlopen(url: str, timeout: int = None):
+    class FakeUrl:
+        @staticmethod
+        def read():
+            return TEXT_STRING
+
+        @staticmethod
+        def readlines():
+            return TEXT_STRING.split()
+    return FakeUrl
+
+
+def test_count_i():
+    with mock.patch('urllib.request.urlopen', new=fake_urlopen):
+        assert count_dots_on_i("some_url") == TEXT_STRING.count("i")
