29e292c8acd5853cc787cde08762549ca3c50788
Alexander Barabanov <Sasha.bar99@gmail.com>
2021-11-26 03:12:19 +0300

    Add tests to check printing to stderr or/and to stdout

---
+++ tests/homework4/test_task3.py
@@ -0,0 +1,35 @@
+import os
+
+import pytest
+
+from homework4.task3 import my_precious_logger
+
+ERROR_STRING = "error: file not found"
+OK_STRING = "OK"
+
+
+@pytest.fixture(scope="function")
+def clean_up():
+    if os.path.exists("stderr.txt"):
+        os.remove("stderr.txt")
+    if os.path.exists("stdout.txt"):
+        os.remove("stdout.txt")
+
+
+def test_output_to_stderr_only(clean_up):
+    my_precious_logger(ERROR_STRING)
+    assert open("stderr.txt").read().strip() == ERROR_STRING and not \
+        os.path.exists("stdout.txt")
+
+
+def test_output_to_stdout_only(clean_up):
+    my_precious_logger(OK_STRING)
+    assert open("stdout.txt").read().strip() == OK_STRING and not \
+        os.path.exists("stderr.txt")
+
+
+def test_output_to_stderr_and_stdout(clean_up):
+    my_precious_logger(ERROR_STRING)
+    my_precious_logger(OK_STRING)
+    assert open("stderr.txt").read().strip() == ERROR_STRING and \
+        open("stdout.txt").read().strip() == OK_STRING
