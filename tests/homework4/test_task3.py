b1890bad5c2c5b3f3e70e2a0d8b8829b63f3284e
Alexander Barabanov <Sasha.bar99@gmail.com>
2021-11-26 03:17:13 +0300

    Add docstrings to test functions

--- tests/homework4/test_task3.py
+++ tests/homework4/test_task3.py
@@ -10,6 +10,7 @@ OK_STRING = "OK"
 
 @pytest.fixture(scope="function")
 def clean_up():
+    """Remove stderr.txt and stdout.txt before every test function."""
     if os.path.exists("stderr.txt"):
         os.remove("stderr.txt")
     if os.path.exists("stdout.txt"):
@@ -17,18 +18,22 @@ def clean_up():
 
 
 def test_output_to_stderr_only(clean_up):
+    """Testing that error message prints only to stderr."""
     my_precious_logger(ERROR_STRING)
     assert open("stderr.txt").read().strip() == ERROR_STRING and not \
         os.path.exists("stdout.txt")
 
 
 def test_output_to_stdout_only(clean_up):
+    """Testing that non-error message prints only to stdout."""
     my_precious_logger(OK_STRING)
     assert open("stdout.txt").read().strip() == OK_STRING and not \
         os.path.exists("stderr.txt")
 
 
 def test_output_to_stderr_and_stdout(clean_up):
+    """Testing that error and non-error messages are printed both to
+    stderr and to stdout."""
     my_precious_logger(ERROR_STRING)
     my_precious_logger(OK_STRING)
     assert open("stderr.txt").read().strip() == ERROR_STRING and \
