63ca2bebe1c5847bdab2687d876b9f5194bc22e3
Alexander Barabanov <Sasha.bar99@gmail.com>
2021-11-29 18:53:53 +0300

    Fix docstring

--- tests/homework2/test_task4.py
+++ tests/homework2/test_task4.py
@@ -6,13 +6,13 @@ def multiply(number1, number2):
 
 
 def test_cache_function():
-    """Testing that cache function returns value of initial function"""
+    """Testing that cache function returns value of initial function."""
     cached = cache(multiply)
     assert cached(100, 200) == multiply(100, 200)
 
 
 def test_cached_values_in_function():
-    """Testing that cache function cashes output of the initial function"""
+    """Testing that cache function cashes output of the initial function."""
     cached = cache(multiply)
     val1 = cached(100, 200)
     val2 = cached(100, 200)
