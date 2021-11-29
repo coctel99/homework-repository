a1da334b535b75bb6b061534daea543487fac745
Alexander Barabanov <Sasha.bar99@gmail.com>
2021-11-29 22:42:45 +0300

    Fix docstring, remove redundant lines

--- homework5/task1.py
+++ homework5/task1.py
@@ -73,27 +73,12 @@ class Teacher:
         self.last_name = last_name
 
     def create_homework(self, text, deadline):
+        """
+        Create new instance of Homework class
+        :param text: Text of the homework
+        :param deadline: Deadline of the homework
+        :return: New Homework class instance
+        """
         deadline = datetime.timedelta(days=deadline)
         created = datetime.datetime.today()
         return Homework(text, deadline, created)
-
-
-if __name__ == '__main__':
-    datetime.date.today()
-    teacher = Teacher('Daniil', 'Shadrin')
-    student = Student('Roman', 'Petrov')
-    print(teacher.first_name)  # Daniil
-    print(student.last_name)  # Petrov
-
-    expired_homework = teacher.create_homework('Learn functions', 0)
-    print(expired_homework.created)  # Example: 2019-05-26 16:44:30.688762
-    print(expired_homework.deadline)  # 0:00:00
-    print(expired_homework.text)  # 'Learn functions'
-
-    # create function from method and use it
-    create_homework_too = teacher.create_homework
-    oop_homework = create_homework_too('create 2 simple classes', 5)
-    print(oop_homework.deadline)  # 5 days, 0:00:00
-
-    student.do_homework(oop_homework)
-    student.do_homework(expired_homework)  # You are late
