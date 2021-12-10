b9c4865a325a2b7481d924528d7220439cd6d60e
Alexander Barabanov <Sasha.bar99@gmail.com>
2021-12-10 23:52:35 +0300

    Add tests showing that original init is applied

--- tests/homework6/test_task1.py
+++ tests/homework6/test_task1.py
@@ -3,17 +3,18 @@ from homework6.task1 import instances_counter
 
 @instances_counter
 class User:
-    pass
+    def __init__(self, name):
+        self.name = name
 
 
 def test_count_created_instances():
     """Testing that created instances are counted."""
-    user, _, _ = User(), User(), User()
-    assert user.get_created_instances() == 3  # 3
+    user, _, _ = User("Alex"), User("Mike"), User("Ivan")
+    assert user.get_created_instances() == 3 and user.name == "Alex"
 
 
 def test_reset_instances_counter():
     """Testing that counter of created instances is being reset."""
-    user, _, _ = User(), User(), User()
-    user.reset_instances_counter()  # 3
-    assert user.get_created_instances() == 0
+    user, _, _ = User("Alex"), User("Mike"), User("Ivan")
+    user.reset_instances_counter()
+    assert user.get_created_instances() == 0 and user.name == "Alex"
