7d1e93d4ca4b5120fc81ef5b0a375095fb13660c
Alexander Barabanov <Sasha.bar99@gmail.com>
2021-12-11 17:00:25 +0300

    Add make key-value storage decorator

--- homework8/task1.py
+++ homework8/task1.py
@@ -1,27 +1,41 @@
-class KeyValueStorage:
-    def __init__(self, path_to_file: str):
+from functools import wraps
+
+
+def make_key_value_storage(cls):
+    """
+    Make key-value storage from specified class
+    :param cls: Class to alter
+    :return: Altered class
+    """
+    original_init = cls.__init__
+
+    @wraps(cls)
+    def new_init(self, path_to_file, *args, **kwargs):
+        """
+        Give instance attributes from file
+        :param self: Instance of a class
+        :param path_to_file: Path to the file with keys and values
+        :param args: Original __init__ *args
+        :param kwargs: Original __init__ **kwargs
+        """
+        original_init(self, *args, **kwargs)
         try:
             with open(path_to_file) as fi:
                 for line in fi:
-                    # TODO: check for value error
                     key, value = line.strip().split("=")
+                    if key.count(" ") > 0:
+                        raise ValueError(key)
                     if getattr(self, key, None) is None:
                         setattr(self, key, value)
         except FileNotFoundError:
             print(f"File {path_to_file} not found")
         except ValueError as val_err:
-            print(f"Cannot assign {val_err.args} attribute")
+            print(f"Cannot assign '{val_err}' attribute")
 
-    def __getitem__(self, item):
+    def get_item(self, item):
+        """Get a value with the key"""
         return getattr(self, item)
 
-
-class B:
-    pass
-
-
-if __name__ == '__main__':
-    a = KeyValueStorage("test.txt")
-    print(a.__dict__)
-    b = B()
-    pass
+    cls.__init__ = new_init
+    cls.__getitem__ = get_item
+    return cls
