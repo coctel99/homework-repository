e2d25605a8a73fe81e89afb44e6b3ee75436ee20
Alexander Barabanov <Sasha.bar99@gmail.com>
2021-12-14 20:06:32 +0300

    Update tests for TableData class methods

--- tests/homework8/test_task2.py
+++ tests/homework8/test_task2.py
@@ -1,64 +1,75 @@
 import sqlite3
 
-from homework8.task2 import make_a_collection
+import pytest
+
+from homework8.task2 import TableData
 
 TEST_DB = './tests/homework8/test.sqlite'
 TEST_TABLE = 'presidents'
 TEST_ORIG_INIT_PARAM = 'Sasha'
 
 
-@make_a_collection
-class A:
-    def __init__(self, name):
-        self.name = name
-
-
-def test_original_init():
-    """Testing that __init__ of original function is not overwritten."""
-    presidents = A(name=TEST_ORIG_INIT_PARAM,
-                   database_name=TEST_DB,
-                   table_name=TEST_TABLE)
-    assert presidents.name == TEST_ORIG_INIT_PARAM
-
-
 def test_get_len():
     """Testing that len() method gives us number of data rows."""
-    presidents = A(name=TEST_ORIG_INIT_PARAM,
-                   database_name=TEST_DB,
-                   table_name=TEST_TABLE)
+    presidents = TableData(TEST_DB, TEST_TABLE)
     assert len(presidents) == 3
+
+
+def test_get_updated_len():
+    """Testing that len() method gives us number of data rows."""
+    presidents = TableData(TEST_DB, TEST_TABLE)
+    conn = sqlite3.connect(TEST_DB)
+    cursor = conn.cursor()
+    cursor.execute(f"insert into {TEST_TABLE} (name, age, country) "
+                   f"values ('Putin', 10000, 'Russia')")
+    conn.commit()
+    assert len(presidents) == 4
+    cursor.execute(f"delete from {TEST_TABLE} where name = 'Putin'")
+    conn.commit()
 
 
 def test_get_data_row():
     """Testing that data row can be got using '[]'."""
-    presidents = A(name=TEST_ORIG_INIT_PARAM,
-                   database_name=TEST_DB,
-                   table_name=TEST_TABLE)
+    presidents = TableData(TEST_DB, TEST_TABLE)
     assert presidents['Yeltsin'] == ('Yeltsin', 999, 'Russia')
+
+
+def test_get_unexisting_data_row():
+    """Testing that ValueError is risen."""
+    presidents = TableData(TEST_DB, TEST_TABLE)
+    with pytest.raises(ValueError):
+        _ = presidents['Barabanov']
+
+
+def test_get_updated_data_row():
+    """Testing that data row is being added from the database."""
+    presidents = TableData(TEST_DB, TEST_TABLE)
+    conn = sqlite3.connect(TEST_DB)
+    cursor = conn.cursor()
+    cursor.execute(f"insert into {TEST_TABLE} (name, age, country) "
+                   f"values ('Putin', 10000, 'Russia')")
+    conn.commit()
+    assert presidents['Putin'] == ('Putin', 10000, 'Russia')
+    cursor.execute(f"delete from {TEST_TABLE} where name = 'Putin'")
+    conn.commit()
 
 
 def test_check_if_exists():
     """Testing that __contains__ method gives True if specified key exists."""
-    presidents = A(name=TEST_ORIG_INIT_PARAM,
-                   database_name=TEST_DB,
-                   table_name=TEST_TABLE)
+    presidents = TableData(TEST_DB, TEST_TABLE)
     assert 'Yeltsin' in presidents
 
 
 def test_check_if_not_exists():
     """Testing that __contains__ method gives False if specified key dont't
      exist."""
-    presidents = A(name=TEST_ORIG_INIT_PARAM,
-                   database_name=TEST_DB,
-                   table_name=TEST_TABLE)
+    presidents = TableData(TEST_DB, TEST_TABLE)
     assert 'Barabanov' not in presidents
 
 
 def test_iterate():
     """Testing that class values can be iterated through."""
-    presidents = A(name=TEST_ORIG_INIT_PARAM,
-                   database_name=TEST_DB,
-                   table_name=TEST_TABLE)
+    presidents = TableData(TEST_DB, TEST_TABLE)
 
     list_of_presidents = []
     for president in presidents:
