0360c90992ec737e6b9e911b61399aad38da79bf
Barabanov Alexander <sasha.bar99@gmail.com>
2022-01-09 18:36:26 +0300

    Rework tests, update punctuation characters number

--- tests/homework2/test_task1.py
+++ tests/homework2/test_task1.py
@@ -1,43 +1,41 @@
-from homework2.task1 import (count_non_ascii_chars, count_punctuation_chars,
-                             get_longest_diverse_words,
-                             get_most_common_non_ascii_char, get_rarest_char)
+from homework2.task1 import read_file
+
+TEXT_FILE = read_file("./tests/homework2/data.txt")
 
 
 def test_longest_diverse_words():
     """Testing that get_longest_diverse_words function gives correct result
     on text"""
-    assert get_longest_diverse_words(
-        "./tests/homework2/data.txt") == ['Bevölkerungsabschub,',
-                                          'Kollektivschuldiger,',
-                                          'Schicksalsfiguren;',
-                                          'résistance-Bewegungen,',
-                                          'unmißverständliche',
-                                          'Entscheidungsschlacht.',
-                                          'Gewissenserforschung,',
-                                          'Machtbewußtsein,',
-                                          'Schicksalsstunde,',
-                                          'Schöpfungsmacht.']
+    assert TEXT_FILE.most_diverse == ['unmißverständliche',
+                                      'Bevölkerungsabschub',
+                                      'Kollektivschuldiger',
+                                      'Werkstättenlandschaft',
+                                      'Schicksalsfiguren',
+                                      'Selbstverständlich',
+                                      'Fingerabdrucks',
+                                      'Friedensabstimmung',
+                                      'außenpolitisch',
+                                      'Seinsverdichtungen']
 
 
 def test_get_rarest_char():
     """Testing that get_rarest_char function gives correct result on text"""
-    assert get_rarest_char("./tests/homework2/data.txt") == "›"
+    assert TEXT_FILE.rarest_char == "›"
 
 
 def test_count_punctuation_chars():
     """Testing that count_punctuation_chars function gives correct result
     on text"""
-    assert count_punctuation_chars("./tests/homework2/data.txt") == 4456
+    assert TEXT_FILE.punct_chars_number == 5475
 
 
 def test_count_non_ascii_chars():
     """Testing that count_non_ascii_chars function gives correct result on
     text"""
-    assert count_non_ascii_chars("./tests/homework2/data.txt") == 2972
+    assert TEXT_FILE.non_ascii_chars_number == 2972
 
 
 def test_get_most_common_non_ascii_char():
     """Testing that get_most_common_non_ascii_char function gives correct
     result on text"""
-    assert get_most_common_non_ascii_char(
-        "./tests/homework2/data.txt") == "ä"
+    assert TEXT_FILE.non_ascii_chars_most_common == "ä"
