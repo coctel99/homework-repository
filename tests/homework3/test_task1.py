daa294c70d1fed42a34b645799a25d652e551962
Barabanov Alexander <sasha.bar99@gmail.com>
2022-01-10 01:31:28 +0300

    Rework multiply function, rework tests for cached results

--- tests/homework3/test_task1.py
+++ tests/homework3/test_task1.py
@@ -1,32 +1,37 @@
 from homework3.task1 import cache_number_of_times
 
 
-@cache_number_of_times(times=2)
-def multiply(number_1, number_2) -> int:
-    return number_1 * number_2
-
-
 def test_cache_multiply_results_2_times():
     """Testing that multiply function results are cached 2 times."""
+    @cache_number_of_times(2)
+    def multiply(number_1: float, number_2: float) -> float:
+        return float(number_1 * number_2)
+
     cached1 = multiply(1, 2)
     cached2 = multiply(1, 2)
     noncached = multiply(1, 2)
-    assert cached1 is cached2 and noncached is None
+    assert cached1 is cached2 and cached2 is not noncached
 
 
-@cache_number_of_times(times=0)
 def test_cache_multiply_results_0_times():
     """Testing that multiply function results are not cached."""
+    @cache_number_of_times(0)
+    def multiply(number_1: float, number_2: float) -> float:
+        return float(number_1 * number_2)
+
     noncached1 = multiply(1, 2)
     noncached2 = multiply(1, 2)
     noncached3 = multiply(1, 2)
-    assert noncached1 and noncached2 and noncached3 is None
+    assert noncached1 is not noncached2 and noncached2 is not noncached3
 
 
-@cache_number_of_times()
 def test_cache_multiply_results_any_times():
     """Testing that multiply function results are cached without
     restrictions."""
+    @cache_number_of_times()
+    def multiply(number_1: float, number_2: float) -> float:
+        return float(number_1 * number_2)
+
     cached1 = multiply(1, 2)
     cached2 = multiply(1, 2)
     cached3 = multiply(1, 2)
