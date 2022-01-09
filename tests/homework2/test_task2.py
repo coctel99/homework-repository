c941964da4f2c9135c773d598da0b408f850afa0
Barabanov Alexander <sasha.bar99@gmail.com>
2022-01-09 22:56:17 +0300

    Fix docstrings for tests

--- tests/homework2/test_task2.py
+++ tests/homework2/test_task2.py
@@ -2,17 +2,17 @@ from homework2.task2 import major_and_minor_elem
 
 
 def test_list_of_even_length():
-    """Testing that list of even length gives a correct result"""
+    """Testing that list of even length gives a correct result."""
     assert major_and_minor_elem([0, 1, 1, 1, 1]) == (1, 0)
 
 
 def test_list_of_odd_length():
-    """Testing that list of odd length gives a correct result"""
+    """Testing that list of odd length gives a correct result."""
     assert major_and_minor_elem([0, 1]) == (0, 1)
 
 
 def test_list_with_negative():
-    """Testing that list with negative numbers gives a correct result"""
+    """Testing that list with negative numbers gives a correct result."""
     assert major_and_minor_elem([-3, -2, -2, -1, -1, -1]) == (-1, -3)
 
 
@@ -22,6 +22,7 @@ def test_list_of_strings():
 
 
 def test_list_of_mixed_chars():
+    """Testing that list with mixed data type values give a correct result."""
     mixed = ["a", "a", "a", 0, 1, 1, "1", "1", -1, -1,
              "-1", "-1", None, None, "None", "None"]
     assert major_and_minor_elem(mixed) == ("a", 0)
