4b4c9b814470b65c7956864a5c3500bf2d01c038
Barabanov Alexander <sasha.bar99@gmail.com>
2022-01-09 22:54:50 +0300

    Add test for values of mixed data types

--- tests/homework2/test_task2.py
+++ tests/homework2/test_task2.py
@@ -13,9 +13,15 @@ def test_list_of_odd_length():
 
 def test_list_with_negative():
     """Testing that list with negative numbers gives a correct result"""
-    assert major_and_minor_elem([-2, -1, -1, -1, -1]) == (-1, -2)
+    assert major_and_minor_elem([-3, -2, -2, -1, -1, -1]) == (-1, -3)
 
 
 def test_list_of_strings():
     """Testing that list with strings gives a correct result"""
     assert major_and_minor_elem(["a", "b", "b", "a", "a"]) == ("a", "b")
+
+
+def test_list_of_mixed_chars():
+    mixed = ["a", "a", "a", 0, 1, 1, "1", "1", -1, -1,
+             "-1", "-1", None, None, "None", "None"]
+    assert major_and_minor_elem(mixed) == ("a", 0)
