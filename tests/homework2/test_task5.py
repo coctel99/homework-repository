61c6e39755e915834e9d30479ded862a36d0c6ad
Barabanov Alexander <sasha.bar99@gmail.com>
2022-01-09 22:38:20 +0300

    Reformat old and add new tests for negative values and inverted ranges

--- tests/homework2/test_task5.py
+++ tests/homework2/test_task5.py
@@ -3,44 +3,90 @@ from homework2.task5 import make_custom_range
 
 def test_custom_range_string():
     """Testing that custom range function makes range from string."""
-    assert make_custom_range("abcdefghijklmnopqrstuvwxyz",
-                             "g") == ['a', 'b', 'c', 'd', 'e', 'f']
+    rng = make_custom_range("abcdefghijklmnopqrstuvwxyz", "g")
+    assert rng == ['a', 'b', 'c', 'd', 'e', 'f']
 
 
 def test_custom_range_string_2_param():
     """Testing that custom range function makes range from one arg
     to another from string."""
-    assert make_custom_range("abcdefghijklmnopqrstuvwxyz",
-                             "g", "p") == ['g', 'h', 'i', 'j', 'k',
-                                           'l', 'm', 'n', 'o']
+    rng = make_custom_range("abcdefghijklmnopqrstuvwxyz", "g", "p")
+    assert rng == ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
 
 
 def test_custom_range_string_3_param():
     """Testing that custom range function makes range from one arg
     to another with specified step from string."""
-    assert make_custom_range("abcdefghijklmnopqrstuvwxyz",
-                             "g", "p", 2) == ['g', 'i', 'k', 'm', 'o']
+    rng = make_custom_range("abcdefghijklmnopqrstuvwxyz","g", "p", 2)
+    assert rng == ['g', 'i', 'k', 'm', 'o']
 
 
 def test_custom_range_int_list():
     """Testing that custom range function makes range from int list."""
-    assert make_custom_range([1, 2, 3, 4, 5, 6, 7], 4) == [1, 2, 3]
+    rng = make_custom_range([1, 2, 3, 4, 5, 6, 7], 4)
+    assert rng == [1, 2, 3]
+
+
+def test_custom_range_negative_int_list():
+    """Testing that custom range function makes range from int list."""
+    rng = make_custom_range([-1, -2, -3, -4, -5, -6, -7], -4)
+    assert rng == [-1, -2, -3]
 
 
 def test_custom_range_int_list_2_param():
     """Testing that custom range function makes range from one arg
     to another from int list."""
-    assert make_custom_range([1, 2, 3, 4, 5, 6, 7], 3, 6) == [3, 4, 5]
+    rng = make_custom_range([1, 2, 3, 4, 5, 6, 7], 3, 6)
+    assert rng == [3, 4, 5]
+
+
+def test_custom_range_negative_int_list_2_param():
+    """Testing that custom range function makes range from one arg
+    to another from int list."""
+    rng = make_custom_range([-1, -2, -3, -4, -5, -6, -7], -3, -6)
+    assert rng == [-3, -4, -5]
 
 
 def test_custom_range_int_list_3_param():
     """Testing that custom range function makes range from one arg
     to another with specified step from int list."""
-    assert make_custom_range([1, 2, 3, 4, 5, 6, 7], 3, 6, 2) == [3, 5]
+    rng = make_custom_range([1, 2, 3, 4, 5, 6, 7], 3, 6, 2)
+    assert rng == [3, 5]
+
+
+def test_custom_range_negative_int_list_3_param():
+    """Testing that custom range function makes range from one arg
+    to another with specified step from int list."""
+    rng = make_custom_range([-1, -2, -3, -4, -5, -6, -7], -3, -6, 2)
+    assert rng == [-3, -5]
 
 
 def test_custom_range_string_list():
     """Testing that custom range function makes range from string list."""
-    assert make_custom_range(["a", "b", "c", "d", "e",
-                              "f", "g", "h", "i", "j"],
-                             "g") == ['a', 'b', 'c', 'd', 'e', 'f']
+    rng = make_custom_range(["a", "b", "c", "d", "e",
+                             "f", "g", "h", "i", "j"], "g")
+    assert rng == ['a', 'b', 'c', 'd', 'e', 'f']
+
+
+def test_custom_range_one_bound_and_step():
+    """Testing that custom range function makes one-bound range with
+    specified step."""
+    rng = make_custom_range([1, 2, 3, 4, 5, 6, 7], bound=4, step=2)
+    assert rng == [1, 3]
+
+
+def test_custom_range_inverted():
+    rng = make_custom_range([1, 2, 3, 4, 5, 6, 7], 6, 2)
+    assert rng == [6, 5, 4, 3]
+
+
+def test_custom_range_inverted_step():
+    rng = make_custom_range([1, 2, 3, 4, 5, 6, 7], 6, 2, -2)
+    assert rng == [6, 4]
+
+
+def test_custom_range_negative_inverted_step():
+    """Testing that custom range function makes range from one arg
+    to another with specified step from int list."""
+    rng = make_custom_range([-1, -2, -3, -4, -5, -6, -7], -6, -2, -2)
+    assert rng == [-6, -4]
