aac9db7b46acca51bdd9743bed4716d2e2845237
Barabanov Alexander <sasha.bar99@gmail.com>
2022-01-10 16:41:08 +0300

    Add test for infinite recursion

--- tests/homework7/test_task1.py
+++ tests/homework7/test_task1.py
@@ -1,5 +1,7 @@
 from homework7.task1 import find_occurrences
 
+recursive_dict = {"key1": "RED"}
+recursive_dict.update({"key2": recursive_dict, "key3": "some_value"})
 example_tree = {
     "first": ["RED", "BLUE"],
     "second": {
@@ -13,6 +15,7 @@ example_tree = {
             "key2": "RED",
             "key3": ["a", "lot", "of", "values", {"nested_key": "RED"}],
             "key4": (0, {1, (2, 3), "RED"}, [4]),
+            "key5": recursive_dict,
         },
     },
     "fourth": "RED",
@@ -22,7 +25,7 @@ example_tree = {
 
 def test_find_occurrences():
     """Testing that 7 occurrences are found in example_tree."""
-    assert find_occurrences(example_tree, "RED") == 7
+    assert find_occurrences(example_tree, "RED") == 8
 
 
 def test_empty_tree():
@@ -33,3 +36,9 @@ def test_empty_tree():
 def test_no_occurrences():
     """Testing that 0 occurrences are found."""
     assert find_occurrences(example_tree, True) == 0
+
+
+def test_recursive_tree():
+    """Testing that no infinite recursion occur on dict with a link to
+    itself."""
+    assert find_occurrences(example_tree, "some_value") == 1
